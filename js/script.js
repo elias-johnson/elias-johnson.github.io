f28186941009d0b5ed30d8cdffd0d1e60f2ec65d
CompoundInterest <ejohnson22@georgefox.edu>
2024-10-28 16:24:01 -0700

    Updating JS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-function decimalToBinary() {
-    // Retrieve the decimal input value from the HTML input element
-    const decimal = document.getElementById('decimalInput').value;
-
-    // Check if the input is a valid number
-    if (decimal === "" || isNaN(decimal)) {
-        document.getElementById('decimalToBinaryResult').innerText = "Please enter a valid number.";
-        return;
-    }
-
-    // Convert the decimal number to binary
-    const binary = (parseInt(decimal, 10) >>> 0).toString(2);
-
-    // Display the result in the paragraph with id "result"
-    document.getElementById('decimalToBinaryResult').innerText = `${binary}`;		
+function decimalToBinary {
+	// Retrieve the decimal input value
+	const decimal = document.getElementById('decimalInput').value;
+	
+	// Check if the input is a valid number
+	if (decimal === "" || isNaN(decimal)) {
+		document.getElementById('decimalToBinaryResult').innerText = "Invalid number.";
+	} else {
+		// Convert the decimal to binary
+		const binary = (parseInt(decimal, 10) >>> 0).toString(2);
+		
+		// Convert to 2s-complement
+		
+		// Display the result
+		document.getElementById('decimalToBinaryResult').innerText = `${binary}`;
+	}	
 }

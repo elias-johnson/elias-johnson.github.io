2e7876afc5ee9e10bceb830530e1d5d0d9282308
CompoundInterest <ejohnson22@georgefox.edu>
2024-10-28 22:45:38 -0700

    JS Testing

--- js/script.js
+++ js/script.js
@@ -8,21 +8,33 @@ function decimalToBinary() {
 		document.getElementById('decimalToBinaryResult').innerText = "Invalid number.";
 	} else {
 		// Convert the decimal to binary
-		if (decimal >= 0) {
-			binary = parseInt(decimal, 10).toString(2);
-			
-			// Add a 0 if the most significant bit is a 1
-			if (binary[0] === '1') {
-				binary = '0' + binary;
-			}	
-			
-			// Sign-extend with 0 if length of binary is not divisible by 4
-			while (binary.length % 4 != 0) {
-				binary = '0' + binary;
+		binary = parseInt(decimal, 10).toString(2);
+		
+		// Add a 0 if the most significant bit is a 1
+		if (binary[0] === '1') {
+			binary = '0' + binary;
+		}	
+		
+		// Sign-extend with 0 if length of binary is not divisible by 4
+		while (binary.length % 4 != 0) {
+			binary = '0' + binary;
+		}
+		
+		// Check if decimal is negative
+		if (decimal < 0) {
+			// Invert the positive binary
+			for (let i = 0; i < binary.length; i++) {
+				if (binary[i] === '0') {
+					binary[i] = 1;
+				} else {
+					binary[i] = 0;
+				}	
 			}
-		} else {
-			const positiveDecimal = Math.abs(decimal);
-			binary = (1 << 8) + positiveDecimal;
+			
+			// Add 1 to the decimal
+			binary = parseInt(binary, 2).toString(10);
+			binary++;
+			binary = parseInt(binary, 10).toString(2);
 			
 			// Remove excess leading bits
 			let index = 0
@@ -38,7 +50,7 @@ function decimalToBinary() {
 			// Sign-extend with 1 if length of binary is not divisible by 4
 			while (binary.length % 4 != 0) {
 				binary = '1' + binary;
-			}			
+			}				
 		}
 
 		// Display the result

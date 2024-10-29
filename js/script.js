ccfc03b9ad39f6dae6688c47b2ed8a20edfd9b1c
CompoundInterest <ejohnson22@georgefox.edu>
2024-10-28 21:14:57 -0700

    JS testing

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
 function decimalToBinary() {
 	// Retrieve the decimal input value
 	const decimal = document.getElementById('decimalInput').value;
+	let binary = "";
 	
 	// Check if the input is a valid number
 	if (decimal === "" || isNaN(decimal)) {
 		document.getElementById('decimalToBinaryResult').innerText = "Invalid number.";
 	} else {
 		// Convert the decimal to binary
-		const binary = (parseInt(decimal, 10) >>> 0).toString(2); // TODO don't actually keep this
-		
-		// Convert to 2s-complement
-		
+		if (decimal >= 0) {
+			binary = parseInt(decimal, 10).toString(2);
+			
+			// Add a 0 if the most significant bit is a 1
+			if (binary[0] === '1') {
+				binary = '0' + binary;
+			}	
+			
+			// Sign-extend with 0 if length of binary is not divisible by 4
+			let remainder = binary.length % 4;
+			for (let i = 0; i < remainder; i++) {
+				binary = '0' + binary;
+			}
+		} else {
+			binary = parseInt(decimal, 10).toString(2);
+			
+			// Remove excess leading bits
+			let index = 0
+			while (index >= 0) {
+				if (binary[index] === '1') {
+					binary = binary.slice(1);
+					index++;
+				} else {
+					index = -1;
+				}	
+			}	
+		}	
+
 		// Display the result
 		document.getElementById('decimalToBinaryResult').innerText = `${binary}`;
 	}	
